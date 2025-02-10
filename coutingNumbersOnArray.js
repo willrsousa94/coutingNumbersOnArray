c3dccebd63e3c1d83b39c21e05c0aec01f85e978
William Sousa <williamrsousa94@hotmail.com>
2025-02-10 15:20:51 -0300

    first update: Added function that create an array of determined lenght with random numbers

--- coutingNumbersOnArray.js
+++ coutingNumbersOnArray.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!')
+function arrayOfRandomOfZeroToTen(number){
+    
+  let arrayRandom = [];
+  for(let i = 0; i < number; i++){
+      arrayRandom.push(Math.random() * 11)
+  }
+
+  console.log(arrayRandom)
+}

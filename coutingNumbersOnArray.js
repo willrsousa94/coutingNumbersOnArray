6eb618109926a4876b8340cddbc2ac0df0552214
William Sousa <williamrsousa94@hotmail.com>
2025-02-10 15:38:36 -0300

    added some conditions to avoid some exceptions e.g min number greater than max number

--- coutingNumbersOnArray.js
+++ coutingNumbersOnArray.js
@@ -10,14 +10,21 @@ function arrayOfRandomOfZeroToTen(number){
 
 function countMinToMaxOfArray(min, max, array){
 
-  let arrayMinToMax = [];
-
-  array.forEach(function(el){
-      if(el >= min && el <= max){
-          arrayMinToMax.push(el);
+  if(min > max){
+      return Cannot `check the result, Min value ${min} is greater than Max value ${max}`
+  } else {
+      let arrayMinToMax = [];
+  
+      array.forEach(function(el){
+          if(el >= min && el <= max){
+              arrayMinToMax.push(el);
+          }
+      });
+      if(min == max){
+          return `There was being generated ${arrayMinToMax.length} equals to ${min} on this array, the array generated was: [${array}]`
+      }else{
+          return `There was being generated ${arrayMinToMax.length} numbers between ${min} and ${max} on this array, the array generated was: [${array}]`
       }
-  });
-
-  return `There was being generated ${arrayMinToMax.length} numbers between ${min} and ${max} on this array, the array generated was: [${array}]`
+  }
 }
 console.log(countEightToTenOfArray(arrayOfRandomOfZeroToTen(20)))
